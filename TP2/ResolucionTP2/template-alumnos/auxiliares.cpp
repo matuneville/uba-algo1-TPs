bcfc184b361c8b709573aab2380469e42dcdb3d1
matuneville <matineville@gmail.com>
2022-10-22 15:31:27 -0300

    pusheo consignas tps

---
+++ TP2/ResolucionTP2/template-alumnos/auxiliares.cpp
@@ -0,0 +1,13 @@
+//
+// Este archivo contiene las definiciones de las funciones auxiliares
+// que ustedes necesiten implementar. Tienen que coincidir con las 
+// declaraciones de auxiliares.h
+//
+
+#include <iostream>
+
+#include "definiciones.h"
+#include "ejercicios.h"
+#include "auxiliares.h"
+
+using namespace std;
